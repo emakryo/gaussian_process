dab122aceeb70acfdc891ef1c2a2a2fbdbcecd8b
Ryosuke Kamesawa <emak.ryo@gmail.com>
2017-06-12 14:29:26 +0900

    Implement privileged kernel

--- test/privEPClassification.py
+++ test/privEPClassification.py
@@ -17,24 +17,23 @@ except:
 
 
 def test0():
-    assert False
     """test with synthesis twin moon data"""
     X, y = data.twin(200)
-    model = EPClassification(sigma=1.0, beta=10)
-    model.fit(X, y)
+    model = privEPClassification()
+    model.fit(X[:,1:], y, X)
 
     Xmesh = np.meshgrid(np.linspace(-2, 2), np.linspace(-1.5, 1.5))
     Xtest = np.stack(Xmesh, 2).reshape(-1, 2)
 
     print(model.log_ml())
     #model.empiricalBayes()
-    print(model.log_ml())
-    decfun = model.decision_function(Xtest)
-    pred = model.predict(Xtest)
+    #print(model.log_ml())
+    decfun = model.decision_function(Xtest[:,1:])
+    pred = model.predict(Xtest[:,1:])
 
     fig, ax = plt.subplots()
-    cs = ax.contourf(Xmesh[0], Xmesh[1], pred.reshape(*Xmesh[0].shape))
-    #cs = ax.contourf(Xmesh[0], Xmesh[1], decfun.reshape(*Xmesh[0].shape))
+    #cs = ax.contourf(Xmesh[0], Xmesh[1], pred.reshape(*Xmesh[0].shape))
+    cs = ax.contourf(Xmesh[0], Xmesh[1], decfun.reshape(*Xmesh[0].shape))
     fig.colorbar(cs)
     ax.plot(X[y == 1, 0], X[y == 1, 1], 'bo',
             X[y == -1, 0], X[y == -1, 1], 'ro')
@@ -45,9 +44,12 @@ def test1():
     X, y = data.australian()
 
     result = []
-    for sigma, beta in [(s, b) for s in 2**np.arange(-5.0, 5.0)
-                        for b in 2**np.arange(-5.0, 5.0)]:
-        accuracy = []
+    for sigmax, betax, sigmaz, betaz, alpha in [(sx, bx, sz, bz, a)
+            for sx in 2**np.arange(-5.0, 5.0, 2.0)
+            for bx in 2**np.arange(-5.0, 5.0, 2.0)
+            for sz in 2**np.arange(-5.0, 5.0, 2.0)
+            for bz in 2**np.arange(-5.0, 5.0, 2.0)
+            for a in np.arange(0.2, 1.2, 0.2)]:
         for xdim in range(2, X.shape[1]):
             for _ in range(5):
                 idx = np.random.permutation(len(y))
@@ -56,8 +58,9 @@ def test1():
                 Xte = X[idx[300:]]
                 yte = y[idx[300:]]
 
-                model = privEPClassification(sigma=sigma, beta=beta)
-                model.fit(Xtr, ytr, Xtr[:,:xdim])
+                model = privEPClassification(sigmax=sigmax, betax=betax,
+                        sigmaz=sigmaz, betaz=betaz, alpha=alpha)
+                model.fit(Xtr[:, :xdim], ytr, Xtr)
                 #model.empiricalBayes()
                 
                 ypr = model.predict(Xtr[:,:xdim])
@@ -65,9 +68,21 @@ def test1():
                 ypr = model.predict(Xte[:,:xdim])
                 test_acc = accuracy_score(yte, ypr)
 
-                result.append({'sigma':sigma, 'beta':beta, 'xdim': xdim,
-                    'privilege': True, 'test_accuracy':test_acc,
+                result.append({'sigmax':sigmax, 'betax':betax, 'xdim':xdim,
+                    'sigmaz':sigmaz, 'betaz':betaz, 'alpha':alpha,
+                    'privilege':True, 'test_accuracy':test_acc,
                     'train_accuracy':train_acc})
+
+    for sigma, beta in [(s, b)
+            for s in 2**np.arange(-5.0, 5.0, 2.0)
+            for b in 2**np.arange(-5.0, 5.0, 2.0)]
+        for xdim in range(2, X.shape[1]):
+            for _ in range(5):
+                idx = np.random.permutation(len(y))
+                Xtr = X[idx[:300]]
+                ytr = y[idx[:300]]
+                Xte = X[idx[300:]]
+                yte = y[idx[300:]]
 
                 model = EPClassification(sigma=sigma, beta=beta)
                 model.fit(Xtr[:,:xdim], ytr)
@@ -79,8 +94,6 @@ def test1():
                 result.append({'sigma':sigma, 'beta':beta, 'xdim': xdim,
                     'privilege': False, 'test_accuracy':test_acc,
                     'train_accuracy':train_acc})
-
-            print(sigma, beta, np.mean(accuracy), np.std(accuracy))
 
     pd.DataFrame(result).to_csv('priv_result')
 
@@ -122,11 +135,12 @@ def debug0():
     np.save("grad_marginal", quiver)
 
 def debug():
-    from gp.kernel import eRBF
-    X = np.random.rand(100, 5)
-    K1 = eRBF(X, X)()
-    K12 = eRBF(X, X[:,:2])()
-    K2 = eRBF(X[:, :2], X[:, :2])()
+    from gp.kernel import privRBF
+    X = np.random.rand(500, 1)
+    Z = np.random.rand(500, 2)
+    K1 = privRBF((X, Z), (X, Z))()
+    K12 = privRBF((X, Z), (X, None), Zsample=Z)()
+    K2 = privRBF((X, None), (X, None), Zsample=Z)()
     K = np.concatenate([
         np.concatenate([K1, K12], axis=1),
         np.concatenate([K12.T, K2], axis=1)])
@@ -134,7 +148,7 @@ def debug():
     print(np.linalg.eigvals(K))
 
 if __name__ == "__main__":
-    test1()
+    test0()
 else:
     X, y = data.twin(10)
     model = EPClassification(X, y)
