197f0b1ae7e2eaff14c55cd4875012aac8ce3fb1
Ryosuke Kamesawa <emak.ryo@gmail.com>
2017-10-10 00:13:18 +0900

    Modify data generator

--- gaussian_process/datasets.py
+++ gaussian_process/datasets.py
@@ -3,11 +3,11 @@ from sklearn.utils import check_random_state
 from gaussian_process.kernel import RBF
 
 
-def generate_gp(n_samples=50, n_features=1, noise=0.01,
+def generate_gp(n_samples=50, n_features=1, noise=0.01, range=(-3, 3),
                 kernel=RBF(sigma=1.0, beta=1.0), random_state=None):
     generator = check_random_state(random_state)
 
-    X = generator.randn(n_samples, n_features)
+    X = generator.rand(n_samples, n_features) * (range[1]-range[0]) + range[0]
     K = kernel(X).K
     y = generator.multivariate_normal(np.zeros(n_samples), K + noise * np.eye(n_samples))
     return X, y
